a0db596399dfa1bde7e33780e7ea7b08be358ba6
Ahmed Rezk <work72019@gmail.com>
2021-07-13 21:37:32 -0600

    Nvim tree.

--- lua/core/nvimtree.lua
+++ lua/core/nvimtree.lua
@@ -14,7 +14,7 @@ M.setup = function()
   vim.o.termguicolors = true
 
   g.nvim_tree_side = "left"
-  g.nvim_tree_width = 30
+  g.nvim_tree_width = 60
   g.nvim_tree_ignore = { ".git", "node_modules", ".cache" }
   g.nvim_tree_auto_open = 1
   g.nvim_tree_auto_close = O.auto_close_tree
